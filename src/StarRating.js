35bd552341361d72f04b9f377986ee0bad03e706
Guilherme <guilherme.martins@alkasoft.com.br>
2024-05-30 15:51:15 -0300

    Ajustes star rating

--- src/StarRating.js
+++ src/StarRating.js
@@ -10,21 +10,27 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margim: "0",
-};
-
-const starStyle = {
-  width: "48px",
-  height: "48px",
-  display: "block",
-  cursor: "pointer",
-};
-
-const StarRating = ({ maxRating = 5 }) => {
-  const [rating, setRating] = useState(0);
+const StarRating = ({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  defaultRating = 0,
+  onSetRating = (rating) => {},
+}) => {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
+
+  const textStyle = {
+    lineHeight: "1",
+    margim: "0",
+    color,
+    fonSize: `${size / 1.5}px`,
+  };
+
+  const handleRating = (rating) => {
+    setRating(rating);
+    onSetRating(rating);
+  };
 
   return (
     <div style={containerStyle}>
@@ -32,10 +38,12 @@ const StarRating = ({ maxRating = 5 }) => {
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onClick={() => setRating(i + 1)}
+            onClick={() => handleRating(i + 1)}
             onMouseEnter={() => setTempRating(i + 1)}
             onMouseLeave={() => setTempRating(0)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            color={color}
+            size={size}
           ></Star>
         ))}
       </div>
@@ -44,7 +52,14 @@ const StarRating = ({ maxRating = 5 }) => {
   );
 };
 
-const Star = ({ onClick, full, onMouseEnter, onMouseLeave }) => {
+const Star = ({ onClick, full, onMouseEnter, onMouseLeave, color, size }) => {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
+
   return (
     <span
       role="button"
@@ -57,8 +72,8 @@ const Star = ({ onClick, full, onMouseEnter, onMouseLeave }) => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -67,7 +82,7 @@ const Star = ({ onClick, full, onMouseEnter, onMouseLeave }) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
